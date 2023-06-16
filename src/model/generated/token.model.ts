9647f70d5f31d8c689182ac4ad05a9c6d8871290
abernatskiy <abernatskiy@toha.li>
2023-06-17 04:10:15 +0900

    Model changes for the new schema
    	modified:   src/model/generated/token.model.ts

--- src/model/generated/token.model.ts
+++ src/model/generated/token.model.ts
@@ -1,4 +1,5 @@
 import {Entity as Entity_, Column as Column_, PrimaryColumn as PrimaryColumn_, ManyToOne as ManyToOne_, Index as Index_, OneToMany as OneToMany_} from "typeorm"
+import * as marshal from "./marshal"
 import {Owner} from "./owner.model"
 import {Transfer} from "./transfer.model"
 
@@ -11,8 +12,8 @@ export class Token {
     @PrimaryColumn_()
     id!: string
 
-    @Column_("int4", {nullable: false})
-    tokenId!: number
+    @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: false})
+    tokenId!: bigint
 
     @Index_()
     @ManyToOne_(() => Owner, {nullable: true})
